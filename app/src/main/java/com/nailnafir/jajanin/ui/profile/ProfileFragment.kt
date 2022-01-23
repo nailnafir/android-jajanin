c88f93b718bc04f9bfab3950487f67edc122fed6
Nailul Firdaus <nail.nafir@gmail.com>
2022-01-23 17:35:11 +0700

    integration with api profile page

--- app/src/main/java/com/nailnafir/jajanin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/nailnafir/jajanin/ui/profile/ProfileFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.gson.Gson
+import com.nailnafir.jajanin.Jajanin
 import com.nailnafir.jajanin.R
+import com.nailnafir.jajanin.model.response.login.User
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
@@ -26,5 +30,19 @@ class ProfileFragment : Fragment() {
 
         view_pager.adapter = sectionPagerAdapter
         tab_layout.setupWithViewPager(view_pager)
+
+        // ambil data dari shared preference yang tersimpan dari login
+        var user = Jajanin.getApp().getUser()
+        var userResponse = Gson().fromJson(user, User::class.java)
+
+        // set data dari user yang tersimpan
+        tv_name.text = userResponse.name
+        tv_email.text = userResponse.email
+
+        if (!userResponse.profile_photo_url.isNullOrEmpty()) {
+            Glide.with(requireActivity())
+                .load(userResponse.profile_photo_url)
+                .into(iv_photo)
+        }
     }
 }
